818e649d6c6349be684a50c3a0f6d007e3db1219
Vincent Raia <vincentraia1@gmail.com>
2021-06-06 17:05:34 -0400

    set up cred.py, sandbox

---
+++ main.py
@@ -0,0 +1,11 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import cred
+scope = "user-read-recently-played"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=cred.client_id, client_secret= cred.client_secret, redirect_uri=cred.redirect_url, scope=scope))
+
+results = sp.current_user_recently_played()
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " – ", track['name'])

5c8389000758ab5e1bd76d999bfa9387126be574
Vincent Raia <vincentraia1@gmail.com>
2021-06-07 22:51:00 -0400

    adding views, init db

--- spotisite/displaymusic/models.py
+++ spotisite/displaymusic/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 # Create your models here.
+
+class Song(models.Model):
+    song_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.song_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
--- spotisite/displaymusic/views.py
+++ spotisite/displaymusic/views.py
@@ -1,4 +1,13 @@
 from django.http import HttpResponse
+from .models import Song
 
 def index(request):
     return HttpResponse("Hello, world. This is where the displays will live.")
+
+def index(request):
+    latest_song_list = Song.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('displaymusic/index.html')
+    context = {
+        'latest_song_list': latest_song_list,
+    }
+    return HttpResponse(template.render(context, request))
